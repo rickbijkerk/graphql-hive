c5ee723cfb13050daf8de555949d07ae702d5389
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-11-19 13:05:36 +0100

    Add /federation-gateway-audit to sitemap.xml (#6001)

--- packages/web/docs/next-sitemap.config.js
+++ packages/web/docs/next-sitemap.config.js
@@ -2,5 +2,13 @@
 export default {
   siteUrl: process.env.SITE_URL || 'https://graphql-hive.com',
   generateIndexSitemap: false,
+  additionalPaths: config => [
+    {
+      loc: config.siteUrl.replace(/\/$/, '') + '/federation-gateway-audit',
+      lastmod: new Date().toISOString(),
+      changefreq: 'weekly',
+      priority: 0.7,
+    },
+  ],
   output: 'export',
 };
