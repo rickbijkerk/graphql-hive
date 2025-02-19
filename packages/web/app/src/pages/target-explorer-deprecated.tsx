390be90688015a9d7673985be7deedf2c9af996d
jdolle <1841898+jdolle@users.noreply.github.com>
2025-02-18 18:46:53 -0800

    fix: show deprecated schema in explorer when there is no usage reported (#6532)

--- packages/web/app/src/pages/target-explorer-deprecated.tsx
+++ packages/web/app/src/pages/target-explorer-deprecated.tsx
@@ -6,7 +6,7 @@ import { SchemaVariantFilter } from '@/components/target/explorer/filter';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import { DateRangePicker, presetLast7Days } from '@/components/ui/date-range-picker';
-import { EmptyList, noSchemaVersion } from '@/components/ui/empty-list';
+import { noSchemaVersion } from '@/components/ui/empty-list';
 import { Link } from '@/components/ui/link';
 import { Meta } from '@/components/ui/meta';
 import { Subtitle, Title } from '@/components/ui/page';
@@ -351,22 +351,9 @@ function ExplorerDeprecatedSchemaPageContent(props: {
   }
 
   const currentOrganization = query.data?.organization?.organization;
-  const hasCollectedOperations = query.data?.hasCollectedOperations === true;
 
   if (!currentOrganization) {
     return null;
-  }
-
-  if (!hasCollectedOperations) {
-    return (
-      <div className="py-8">
-        <EmptyList
-          title="Hive is waiting for your first collected operation"
-          description="You can collect usage of your GraphQL API with Hive Client"
-          docsUrl="/features/usage-reporting"
-        />
-      </div>
-    );
   }
 
   return (
